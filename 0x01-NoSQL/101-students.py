d914c03758fd2e1a31d9d66b1ef44f8f278bb112
tami-cp0 <findtamilore>
2024-06-16 23:43:01 +0100

    advanced tasks done

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -3,3 +3,15 @@
 
 
 def top_students(mongo_collection):
+    """ Returns all students sorted by average score"""
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
