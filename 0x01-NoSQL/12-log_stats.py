5c63232519e73e9fc8f779ceaadef5afee24d4de
tami-cp0 <findtamilore>
2024-06-16 16:34:27 +0100

    task 12 update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,28 +4,29 @@ Script  that provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
+if __name__ == '__main__':
+    """Prints the log stats in nginx collection"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-logs = client.logs.nginx
+    # number of logs
+    count = logs.count_documents({})
 
-# number of logs
-count = logs.count_documents({})
+    get = len([log for log in logs.find({"method": "GET"})])
+    post = len([log for log in logs.find({"method": "POST"})])
+    put = len([log for log in logs.find({"method": "PUT"})])
+    patch = len([log for log in logs.find({"method": "PATCH"})])
+    delete = len([log for log in logs.find({"method": "DELETE"})])
 
-get = len([log for log in logs.find({"method": "GET"})])
-post = len([log for log in logs.find({"method": "POST"})])
-put = len([log for log in logs.find({"method": "PUT"})])
-patch = len([log for log in logs.find({"method": "PATCH"})])
-delete = len([log for log in logs.find({"method": "DELETE"})])
+    doc = len([log for log in logs.find({"method": "GET", "path": "/status"})])
 
-doc = len([log for log in logs.find({"method": "GET", "path": "/status"})])
-
-print(
-    f"{count}\n"
-    "Methods:\n"
-    f"\t method GET: {get}\n"
-    f"\t method POST: {post}\n"
-    f"\t method PUT: {put}\n"
-    f"\t method PATCH: {patch}\n"
-    f"\t method DELETE: {delete}\n"
-    f"{doc} status check"
-)
+    print(
+        f"{count}\n"
+        "Methods:\n"
+        f"\t method GET: {get}\n"
+        f"\t method POST: {post}\n"
+        f"\t method PUT: {put}\n"
+        f"\t method PATCH: {patch}\n"
+        f"\t method DELETE: {delete}\n"
+        f"{doc} status check"
+    )
